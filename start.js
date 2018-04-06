6b8326a2fe478b80b5a9799609d724647be98737
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-04-05 22:12:17 -0400

    Append to file if exists, else open write stream and write to file

--- start.js
+++ start.js
@@ -51,8 +51,15 @@ publicIp.v4().then(ip => {
         batNode.kadenceNode.iterativeStore(fileName, [batNode.kadenceNode.identity.toString(), batNode.kadenceNode.contact], (err, stored) => {
           console.log('nodes who stored this value: ', stored)
           let fileContent = new Buffer(receivedData.fileContent)
-          let writeStream = fs.createWriteStream(`./hosted/${fileName}`, {flags: 'a'})
-          writeStream.write(fileContent)
+          fs.exists(`./hosted/${fileName}`, (exists) => {
+            if (exists){
+              let writeStream = fs.createWriteStream(`./hosted/${fileName}`, {flags: 'a'})
+              writeStream.write(fileContent)
+            } else {
+              let writeStream = fs.createWriteStream(`./hosted/${fileName}`)
+              writeStream.write(fileContent)
+            }
+          })
         })
       } else if (receivedData.messageType === "AUDIT_FILE") {
         fs.exists(`./hosted/${receivedData.fileName}`, (doesExist) => {
